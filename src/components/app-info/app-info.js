f758ea4b2412e780b54d937b2e4549a388cc63ec
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-04-22 19:53:25 +0700

    an application template has been created using the React library

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Accounting for employee salaries in the company</h1>
+            <h2>Number of employees: </h2>
+            <h2>Employees with a bonus: </h2>
+        </div>
+    )
+}
+
+export default AppInfo
---
+++ src/components/app-search/app-search.js
@@ -0,0 +1,13 @@
+import './app-search.css';
+
+const SearchPanel = () => {
+    return (
+        <input 
+            type="text" 
+            className="form-control search-input"
+            placeholder="Find an employee"
+        />
+    )
+}
+
+export default SearchPanel;
