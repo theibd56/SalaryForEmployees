0183beffe2feae702e324c249e220d175aec75d0
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-04-27 20:46:43 +0700

    create filter for employees

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,23 +1,30 @@
 import './app-filter.css';
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+
+    const buttonsData = [
+        {name: 'all', label: 'All employees'},
+        {name: 'raise', label: 'For a raise'},
+        {name: 'small-salary', label: 'The salary is less than $1000'},
+        {name: 'big-salary', label: 'Salary is more than $3000'},
+    ]
+
+    const buttons = buttonsData.map(({name, label}) => {
+        const active = props.filter === name;
+        const btnClass = active ? 'btn-light' : 'btn-outline-light';
+        return (
+            <button type='button'
+                    className={`btn ${btnClass}`}
+                    key={name}
+                    onClick={() => props.onUpdateFilter(name)}>
+                        {label}
+            </button>
+        )
+    })
+
     return (
         <div className="btn-group">
-            <button 
-                className="btn btn-light"
-                type="button">
-                    All employees
-            </button>
-            <button 
-                className="btn btn-outline-light"
-                type="button">
-                    For a raise
-            </button>
-            <button 
-                className="btn btn-outline-light"
-                type="button">
-                    Salary is more than $ 1000
-            </button>
+            {buttons}
         </div>
     )
 }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,9 @@ class App extends Component {
                 {id: 1, name: 'Alex C.', salary: 800, increase: true, rise: false},
                 {id: 2, name: 'Corney J.', salary: 1600, increase: false, rise: true},
                 {id: 3, name: 'Mike F.', salary: 2500, increase: false, rise: false},
-            ]
+            ],
+            term: '',
+            filter: 'all'
         }
         this.maxId = 4;
     }
@@ -50,22 +52,55 @@ class App extends Component {
                 return item;
             })
         }))
-    } 
+    }
+
+    searchEmployees = (items, term) => {
+        if(term.length === 0) {
+            return items
+        }
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term})
+    }
+
+    onFilter = (items, filter) => {
+        switch(filter) {
+            case 'raise':
+                return items.filter(item => item.rise)
+            case 'small-salary':
+                return items.filter(item => item.salary <= 1000)
+            case 'big-salary':
+                return items.filter(item => item.salary > 3000)
+            default:
+                return items
+        }
+    }
+
+    onUpdateFilter = (filter) => {
+        this.setState({filter})
+    }
 
     render() {
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increasedEmployees = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.onFilter(this.searchEmployees(data, term), filter);
+
         return (
             <div className="app">
                 <AppInfo employees={employees} increasedEmployees={increasedEmployees}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter ={filter} onUpdateFilter={this.onUpdateFilter}/>
                 </div>
     
                 <EmployeesList
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteEmployees}
                     onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.newEmployees}/>
